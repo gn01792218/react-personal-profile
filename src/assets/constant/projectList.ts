d6da6af7f3ea33ec4bc1deb193ba00d79c6b8aae
MSI\gn01792218 <gn01792218@hotmail.com>
2023-06-12 15:19:50 +0800

    add projects repoUrl

--- src/assets/constant/projectList.ts
+++ src/assets/constant/projectList.ts
@@ -5,42 +5,49 @@ export const projectList:Partial<Project>[] = [
         title:"急急如律令，任意速速簽",
         demoUrl:"https://gn01792218.github.io/f2e-react-sign-system/",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/122/702/original/9c98c6a5f734ac1f4d7d9e0d5af4b696ad96701b.jpg"],
-        tags:["Vite","React","Typscript","TailwindCss","Redux","React-router-dom"]
+        tags:["Vite","React","Typscript","TailwindCss","Redux","React-router-dom"],
+        repoUrl:"https://github.com/gn01792218/f2e-react-sign-system"
     },
     {
         title:"自製元件集成網",
         demoUrl:"https://gn01792218.github.io/vue3-tailwind-component/#/",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/359/original/b7dec1f0d5b749fd391d6b68ddb2537db7771c39.jpg"],
-        tags:["Vite","vue3","typscript","TailwindCss"]
+        tags:["Vite","vue3","typscript","TailwindCss"],
+        repoUrl:"https://github.com/gn01792218/vue3-tailwind-component"
     },
     {
         title:"TSZ大冒險旅遊資訊網",
         demoUrl:"https://gn01792218.github.io/f2e-tourism/",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/360/original/e413f0db6b9a7f5afc49fab4034958d84b7bf830.jpg"],
-        tags:["Vue CLI","Vue3","Typscript","Bootstrap5","SCSS","Vuex","Vue-Router"]
+        tags:["Vue CLI","Vue3","Typscript","Bootstrap5","SCSS","Vuex","Vue-Router"],
+        repoUrl:"https://github.com/gn01792218/f2e-tourism"
     },
     {
         title:"公車任意門",
         demoUrl:"https://gn01792218.github.io/f2e-bus/#/",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/361/original/677d89de98ce75bc366cf8c05f3fe37a3956dd25.jpg"],
-        tags:["Vue CLI","Vue3","Typscript","Bootstrap5","SCSS","Vuex","Vue-Router"]
+        tags:["Vue CLI","Vue3","Typscript","Bootstrap5","SCSS","Vuex","Vue-Router"],
+        repoUrl:"https://github.com/gn01792218/f2e-bus"
     },
     {
         title:"小孩益智遊戲平台",
         demoUrl:"https://gn01792218.github.io/memory-card/#/",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/367/original/8ae266ff6d14d241d7d770fcb4cd8348c15d7e06.jpg"],
-        tags:["Vite","Vue3","Typscript","TailwindCss","Vue-Router","Vuex"]
+        tags:["Vite","Vue3","Typscript","TailwindCss","Vue-Router","Vuex"],
+        repoUrl:"https://github.com/gn01792218/memory-card"
     },
     {
         title:"溫水煮程式",
         demoUrl:"https://www.youtube.com/watch?v=yKF2I6CJ4ns&t=5s",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/362/original/9fac2bf6af059da1a76ed4a1681de6ac9c9a27b0.jpg"],
-        tags:["Vue CLI","Vue2","Bootstrap","SCSS","Vue-Router"]
+        tags:["Vue CLI","Vue2","Bootstrap","SCSS","Vue-Router"],
+        repoUrl:"https://github.com/gn01792218/vue2-webProject"
     },
     {
         title:"WaveDefence - Java塔防自走棋遊戲",
         demoUrl:"https://www.youtube.com/watch?v=l6ddYNVO8Cs",
         imgs:["https://s3cdn.yourator.co/attachments/attachments/000/111/363/original/6c13b7ea4b93cf80a1efb861fbefb1147b6bafec.jpg"],
-        tags:["Java"]
+        tags:["Java"],
+        repoUrl:"https://github.com/gn01792218/Wave-Defence"
     }
 ] 
