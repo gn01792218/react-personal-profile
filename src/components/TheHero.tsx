ffbe608a16111f10921da10266376b04de685e53
MSI\gn01792218 <gn01792218@hotmail.com>
2023-06-17 16:42:37 +0800

    modify TheHero Img RWD

--- src/components/TheHero.tsx
+++ src/components/TheHero.tsx
@@ -10,7 +10,7 @@ function TheHero() {
             <section className="z-10">
                 <TheNavigator />
             </section>
-           <div className="relative w-[150px] sm:w-[300px] before:absolute before:z-10 before:top-5 before:w-[200px] before:h-[200px] sm:before:w-[400px] sm:before:h-[400px] before:bg-primary-300 before:shadow-x before:rounded-full">
+           <div className="relative top-[20%] w-[150px] sm:top-0 sm:w-[300px] before:absolute before:z-10 before:top-5 before:w-[200px] before:h-[200px] sm:before:w-[400px] sm:before:h-[400px] before:bg-primary-300 before:shadow-x before:rounded-full">
              <img className="hero-img w-full absolute z-10" src={getAssetsFileURL('me_noBg.png')} alt="Jacky Hung" />
              <p className="hero-title absolute z-10 text-primary-400 font-extrabold  text-6xl sm:text-8xl font-serif">Front End</p>
            </div>
