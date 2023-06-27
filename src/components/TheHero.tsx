69936bca854b52bb9655fa4ce13527db2fc75cea
MSI\gn01792218 <gn01792218@hotmail.com>
2023-06-27 14:11:37 +0800

    bug fix

--- src/components/TheHero.tsx
+++ src/components/TheHero.tsx
@@ -11,7 +11,7 @@ function TheHero() {
                 {/* <TheNavigator /> */}
             </section>
            <div className="relative mt-5 top-[20%] w-[150px] sm:top-0 sm:w-[300px] before:absolute before:z-10 before:top-5 before:w-[200px] before:h-[200px] sm:before:w-[400px] sm:before:h-[400px] before:bg-primary-300 before:shadow-x before:rounded-full">
-             <img className="hero-img w-full absolute z-10" src={getAssetsFileURL('me_noBg.webp')} aJulietlt="Jacky Hung" />
+             <img className="hero-img w-full absolute z-10" src={getAssetsFileURL('me_noBg.webp')} alt="Jacky Hung" />
              <p className="hero-title absolute z-10 text-primary-400 font-extrabold  text-6xl sm:text-8xl font-serif">Front End</p>
            </div>
         </div>
