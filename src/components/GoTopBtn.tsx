d2574346362da3d0a08a6f0d7bbc8caff454818d
MSI\gn01792218 <gn01792218@hotmail.com>
2023-07-09 15:53:04 +0800

    add fotter copyright

--- src/components/GoTopBtn.tsx
+++ src/components/GoTopBtn.tsx
@@ -31,7 +31,7 @@ function GoTopBtn() {
             type="button"
             data-te-ripple-init
             data-te-ripple-color="light"
-            className="hidden fixed z-10 bottom-5 right-5 inline-block rounded-full
+            className="hidden fixed z-[999] bottom-5 right-5 inline-block rounded-full
             bg-success-700 p-2 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out 
             hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] 
             focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 
--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,8 +1,11 @@
-
 function TheFooter() {
     return (
-        <div className='h-[100px] text-white bg-orange-400'>
-        </div>
+        <section className='relative w-full flex justify-center h-[150px] text-white'>
+            <div className="wave" id="wave1">
+            </div>
+            <div className="wave" id="wave2"></div>
+            <h1 className="absolute z-10 bottom-2 text-primary-100">Copyright © 2023 洪夢澤 版權所有</h1>
+        </section>
     )
 }
 export default TheFooter
