2d318069d7fd946847fa44cbf65a03899a1d6671
MSI\gn01792218 <gn01792218@hotmail.com>
2023-07-27 16:16:26 +0800

    [vite] add vite build plugin

--- dist/assets/About.d108542d.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.bc73e2d6.js";function u(){return t("div",{className:"text-white",children:"\u9019\u662FAbout"})}export{u as default};
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import AutoImport from 'unplugin-auto-import/vite'
+import viteCompression from 'vite-plugin-compression'
+import viteImagemin from 'vite-plugin-imagemin'
 const {resolve} = require('path')
 
 // https://vitejs.dev/config/
@@ -10,7 +12,13 @@ export default defineConfig({
     AutoImport({
       imports:['react', 'react-router-dom'],
       dts:'src/auto-imports.d.ts'
-    })
+    }),
+    viteCompression({
+        algorithm:'gzip',
+        threshold:10240,  //文件大於多少b才壓縮( 10240b即10kb )
+        deleteOriginFile:false, //是否要壓縮完後刪除原檔案
+    }),
+    viteImagemin()
   ],
   define:{
     'process.env':{}
